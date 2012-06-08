4f8f0fbd7d8c92b7f1d2bf556d1cb7ddbff5a468
Christopher Best <bestchris@gmail.com>
2012-06-08 14:27:02 -0400

    fuller attribute statement implimentation

--- SoarDOT/SoarDOT/AttributeStatement.cs
+++ SoarDOT/SoarDOT/AttributeStatement.cs
@@ -14,16 +14,31 @@ namespace SoarDOT
 			OT_Edge
 		};
 
+		public static Dictionary<EObjectType, string> typeLabels = new Dictionary<EObjectType, string> {
+			{EObjectType.OT_Edge, "edge"},
+			{EObjectType.OT_Node, "node"},
+			{EObjectType.OT_Graph, "graph"}
+		};
+
 		// the type of object for which to declare attributes
 		public EObjectType objectType;
 
 		// the list of attributes
 		public List<GraphAttribute> attributes = new List<GraphAttribute>();
 
+		// add an attribute
+		public void AddAttribute(GraphAttribute attribute)
+		{
+			attributes.Add(attribute);
+		}
+
 		// render to a string
 		public string Render()
 		{
 			StringBuilder definition = new StringBuilder();
+
+			// add type declaration
+			definition.Append(typeLabels[objectType]);
 
 			// add opening square bracket
 			definition.Append("[ ");
