2c82122a6e5e0cf6e6d4c948d50ffd8ebcb99d98
Christopher Best <bestchris@gmail.com>
2012-06-11 16:07:41 -0400

    Added GUI project
    Started hooking GUI up to SML

---
+++ SoarDOT/SoarViz/MainView.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SoarViz
+{
+	partial class MainView
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TextView = new System.Windows.Forms.TextBox();
+			this.ConnectButton = new System.Windows.Forms.Button();
+			this.AgentList = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// TextView
+			// 
+			this.TextView.Location = new System.Drawing.Point(9, 38);
+			this.TextView.Multiline = true;
+			this.TextView.Name = "TextView";
+			this.TextView.Size = new System.Drawing.Size(503, 378);
+			this.TextView.TabIndex = 0;
+			// 
+			// ConnectButton
+			// 
+			this.ConnectButton.Location = new System.Drawing.Point(12, 9);
+			this.ConnectButton.Name = "ConnectButton";
+			this.ConnectButton.Size = new System.Drawing.Size(93, 23);
+			this.ConnectButton.TabIndex = 1;
+			this.ConnectButton.Text = "Connect to Soar";
+			this.ConnectButton.UseVisualStyleBackColor = true;
+			this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+			// 
+			// AgentList
+			// 
+			this.AgentList.FormattingEnabled = true;
+			this.AgentList.Location = new System.Drawing.Point(111, 9);
+			this.AgentList.Name = "AgentList";
+			this.AgentList.Size = new System.Drawing.Size(121, 21);
+			this.AgentList.TabIndex = 2;
+			this.AgentList.SelectedIndexChanged += new System.EventHandler(this.AgentList_SelectedIndexChanged);
+			// 
+			// MainView
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(520, 425);
+			this.Controls.Add(this.AgentList);
+			this.Controls.Add(this.ConnectButton);
+			this.Controls.Add(this.TextView);
+			this.Name = "MainView";
+			this.Text = "Main View";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox TextView;
+		private System.Windows.Forms.Button ConnectButton;
+		private System.Windows.Forms.ComboBox AgentList;
+	}
+}
+
---
+++ SoarDOT/SoarViz/MainView.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SoarViz
+{
+	public partial class MainView : Form
+	{
+		private static sml.Kernel kernel = null;
+		public static sml.Agent agent = null;
+		List<string> agentNames = new List<string>();
+
+		public MainView()
+		{
+			InitializeComponent();
+			// set drop-down data source
+			//AgentList.DataSource = agentNames;
+		}
+
+		private void ConnectButton_Click(object sender, EventArgs e)
+		{
+			string x = System.IO.Directory.GetCurrentDirectory();
+			// create connection to Soar
+			kernel = sml.Kernel.CreateRemoteConnection();
+
+			// report failed connection
+			if (kernel.HadError())
+			{
+				TextView.Text = "Error connecting to Soar";
+			}
+
+			// read list of agents
+			agentNames.Clear();
+			for (int i = 0; i < kernel.GetNumberAgents(); i++)
+			{
+				agentNames.Add(kernel.GetAgentByIndex(i).GetAgentName());
+				// TODO this should be able to read from agentNames but it's not
+			}
+			// select first agent if it exists
+			if (kernel.GetNumberAgents() > 0)
+			{
+				// TODO I don't we have to set agent here because SelectedIndexChanged will fire
+				agent = kernel.GetAgentByIndex(0);
+				AgentList.SelectedIndex = 0;
+			}
+
+			sml.ConnectionInfo ci = kernel.GetConnectionInfo(0);
+		}
+
+		private void AgentList_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			agent = kernel.GetAgentByIndex(AgentList.SelectedIndex);
+			if (agent == null)
+			{
+				TextView.Text = "Error selecting agent";
+				return;
+			}
+
+			// show agent info
+			TextView.Text = agent.GetCurrentPhase().ToString();
+		}
+	}
+}
